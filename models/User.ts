781fbaedb58895c27b3f528fb62a8517bb40af36
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-11-13 04:28:53 +0530

    corrected the location of User model

--- models/User.ts
+++ models/User.ts
@@ -1,8 +1,9 @@
+// models/User.ts
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
