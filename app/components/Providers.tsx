21070de1a90adbfd3e2f200dd3bfe84cdebceb6c
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-11-13 03:22:00 +0530

    Add NextAuth Provider in Providers.tsx

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import React from "react";
+import { SessionProvider } from "next-auth/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // later: add SessionProvider, ThemeProvider, Toast, etc.
-  return <>{children}</>;
+  return <SessionProvider>{children}</SessionProvider>;
 }
