76b472ea70b8c69b7294db109c76234855aa2888
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-11-13 03:35:15 +0530

    replaced auth options import

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,5 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        // 🚨 FOR NOW ONLY — Static login (We will replace with MongoDB later)
-        if (
-          credentials?.email === "test@test.com" &&
-          credentials?.password === "123"
-        ) {
-          return { id: "1", email: "test@test.com" };
-        }
-
-        return null;
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/login",
-  },
-};
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
