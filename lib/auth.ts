f3cd3624bdedd7072da7c02141d2f55f884124de
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-11-13 03:30:14 +0530

    lib/auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,29 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (
+          credentials?.email === "test@test.com" &&
+          credentials?.password === "123"
+        ) {
+          return { id: "1", email: "test@test.com" };
+        }
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
